915ff25d9722db644ed408530c059d49f9a6e9d1
MinuParkBasler <minu.park@baslerweb.com>
2023-04-05 17:54:47 +0900

    Updated

--- main.cpp
+++ main.cpp
@@ -9,13 +9,11 @@ int main(int argc, char *argv[])
     Qylon::Qylon qy;
     MainWindow w;
 
-
-
-
-
-
     auto obj = qy.addCamera();
 //    obj->openCamera("Basler acA1300-60gm (24070434)");
+    auto engine = qy.addQDC();
+
+    engine->addSequence(Qylon::Sequence_Threshold);
 
     w.set(obj);
 
