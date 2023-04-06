391de7c0c111827fb13e9f5b2a682deb7000a7ac
MinuParkBasler <minu.park@baslerweb.com>
2023-04-06 17:10:45 +0900

    A

--- main.cpp
+++ main.cpp
@@ -9,15 +9,24 @@ int main(int argc, char *argv[])
     Qylon::Qylon qy;
     MainWindow w;
 
-    auto obj = qy.addCamera();
+//    auto obj = qy.addCamera();
 //    obj->openCamera("Basler acA1300-60gm (24070434)");
-    auto engine = qy.addQDC();
 
-    engine->addSequence(Qylon::Sequence_Threshold);
+//    auto grabber = qy.addGrabber();
 
-    w.set(obj);
+//    grabber->loadApplet("Cleanc_AF_VCL_Mono8_V1.hap");
+//    grabber->loadConfiguration("/home/minwoo/Projects/Qylon/Acquisition/Cleanc_AF_VCL_Mono8_V1.hap");
+//    grabber->initialize();
 
-    QObject::connect(obj, SIGNAL(grabbed()), &w, SLOT(get()));
+
+
+//    engine->addSequence(Qylon::Sequence_Threshold);
+
+
+
+//    w.set(obj);
+
+//    QObject::connect(obj, SIGNAL(grabbed()), &w, SLOT(get()));
 
     w.show();
     return a.exec();
